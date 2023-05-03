d4f7a3222dae1792f9ef085217fff68718d3268c
SteveRab3 <stevensrabehaja@icloud.com>
2023-05-03 10:59:16 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # Params
+
+`IParameterCategory` Interface Methods
+--------------------------------------
+
+#### `getLabel()`
+
+Returns the **label** of the category.
+
+Example: `category.getLabel(); // returns "General Settings"`
+
+#### `getDescription()`
+
+Returns the **description** of the category.
+
+Example: `category.getDescription(); // returns "Settings that affect the behavior of the application as a whole"`
+
+#### `addParameterItem(IParameterItem<?> parameterItem)`
+
+Adds a **parameter item** to the category.
+
+Example: `category.addParameterItem(new StringParameter("username", "Username", "The username of the user"));`
+
+#### `removeParameterItem(IParameterItem<?> parameterItem)`
+
+Removes a **parameter item** from the category.
+
+Example: `category.removeParameterItem(usernameParameter);`
+
+#### `getParameterItems()`
+
+Returns a **map of all parameter items** in the category, keyed by their names.
+
+Example: `Map<String, IParameterItem<?>> parameterItems = category.getParameterItems();`
+
+#### `getParentCategory()`
+
+Returns an **optional** containing the **parent category** of this category, or an empty optional if this category is the root category.
+
+Example: `Optional<IParameterCategory> parentCategory = category.getParentCategory();`
+
+#### `setChildCategory(IParameterCategory childCategory)`
+
+Sets the **child category** of this category.
+
+Example: `category.setChildCategory(new ParameterCategory("Advanced Settings", "Settings for advanced users"));`
+
+#### `setParentCategory(IParameterCategory parentCategory)`
+
+Sets the **parent category** of this category.
+
+Example: `category.setParentCategory(parentCategory);`
+
+#### `isRoot()`
+
+Returns **true** if this category is the root category, **false** otherwise.
+
+Example: `boolean isRoot = category.isRoot();`
+
+#### `getLevel()`
+
+Returns the **level** of this category in the hierarchy.
+
+Example: `ParamaterLevel level = category.getLevel();`
+
+#### `getAllParentCategories()`
+
+Returns a **list of all parent categories** of this category, in order from the root category to the immediate parent of this category.
+
+Example: `List<IParameterCategory> parentCategories = category.getAllParentCategories();`
+
+#### `getChildCategoryList()`
+
+Returns a **list of child categories**
+
+Example: `List<IParameterCategory> childCategories = category.getChildCategoryList();`
+
+#### `getAllParentParameterItems()`
+
+Returns a **map of all parameter items** in this category and all its parent categories, keyed by their names.
+
+Example: `Map<String, IParameterItem<?>> allParameterItems = category.getAllParentParameterItems();`
--- src/main/java/com/iloo/params/core/IParameterCategory.java
+++ src/main/java/com/iloo/params/core/IParameterCategory.java
@@ -6,31 +6,122 @@ import java.util.Optional;
 
 import org.eclipse.jdt.annotation.NonNull;
 
+/**
+ *
+ * This interface represents a parameter category, which can contain parameter
+ * items and other categories.
+ *
+ * It provides methods for getting and setting information about the category
+ * and its contents, as well as methods for navigating the category hierarchy.
+ */
 public interface IParameterCategory {
 
+	/**
+	 *
+	 * Returns the label of the category.
+	 *
+	 * @return the label of the category
+	 */
 	String getLabel();
 
+	/**
+	 *
+	 * Returns the description of the category.
+	 *
+	 * @return the description of the category
+	 */
 	String getDescription();
 
+	/**
+	 *
+	 * Adds a parameter item to the category.
+	 *
+	 * @param parameterItem the parameter item to add
+	 */
 	void addParameterItem(@NonNull IParameterItem<?> parameterItem);
 
+	/**
+	 *
+	 * Removes a parameter item from the category.
+	 *
+	 * @param parameterItem the parameter item to remove
+	 */
 	void removeParameterItem(@NonNull IParameterItem<?> parameterItem);
 
+	/**
+	 *
+	 * Returns a map of all parameter items in the category, keyed by their names.
+	 *
+	 * @return a map of parameter items in the category
+	 */
 	Map<String, IParameterItem<?>> getParameterItems();
 
+	/**
+	 *
+	 * Returns an optional containing the parent category of this category, or an
+	 * empty optional if this category is the root category.
+	 *
+	 * @return an optional containing the parent category, or an empty optional if
+	 *         this category is the root category
+	 */
 	Optional<IParameterCategory> getParentCategory();
 
+	/**
+	 *
+	 * Sets the child category of this category.
+	 *
+	 * @param childCategory the child category to set
+	 */
 	void setChildCategory(@NonNull IParameterCategory childCategory);
 
+	/**
+	 *
+	 * Sets the parent category of this category.
+	 *
+	 * @param parentCategory the parent category to set
+	 */
 	void setParentCategory(@NonNull IParameterCategory parentCategory);
 
+	/**
+	 *
+	 * Returns true if this category is the root category, false otherwise.
+	 *
+	 * @return true if this category is the root category, false otherwise
+	 */
 	boolean isRoot();
 
+	/**
+	 *
+	 * Returns the level of this category in the hierarchy.
+	 *
+	 * @return the level of this category in the hierarchy
+	 */
 	ParamaterLevel getLevel();
 
+	/**
+	 *
+	 * Returns a list of all parent categories of this category, in order from the
+	 * root category to the immediate parent of this category.
+	 *
+	 * @return a list of all parent categories of this category
+	 */
 	List<IParameterCategory> getAllParentCategories();
 
+	/**
+	 *
+	 * Returns a list of all child categories of this category.
+	 *
+	 * @return a list of all child categories of this category
+	 */
 	List<IParameterCategory> getChildCategoryList();
 
+	/**
+	 *
+	 * Returns a map of all parameter items in all parent categories of this
+	 * category, keyed by their names.
+	 *
+	 * @return a map of all parameter items in all parent categories of this
+	 *         category
+	 */
 	Map<String, IParameterItem<?>> getAllParentParameterItems();
 }
